105149d42fc83c8f5dad0c3674c324024b80d836
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-10-31 18:45:08 +0545

    add: connection to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
+import { getEnvironmentVariables } from "./environments/environment";
 
 let app: express.Application = express();
 
@@ -6,18 +8,7 @@ app.listen(3000, () => {
     console.log("server running at port 3000");
 });
 
-// learning spread operator
-let obj1={id:1,name:'apple'}
-console.log(obj1)
-let obj2=obj1 // this creates reference to same object
-obj2.name='ball'
-console.log(obj1)
-
-let obj3={...obj1,name:'cat',address:'ktm'}
-console.log(obj1); // obj 1 is unaffected as ... makes a copy of it
-console.log(obj3);
-
-let arr1 =[1,2,3]
-let arr2 =[4,5,6]
-let arr3=[...arr1,...arr2,-1,-2]
-console.log(arr3);
+mongoose
+    .connect(getEnvironmentVariables().db_uri)
+    .then(() => console.log("connected to mongodb"))
+    .catch((err) => console.log(err));
