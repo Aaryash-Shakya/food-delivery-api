2721728d622793667086ec2c9980e092068c2abf
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-11-08 19:00:45 +0545

    add: nodemailer with mailtrap to test email

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import userModel from "../models/userModel";
+import { sendEmail } from "../utils/nodeMailer";
 import { Utils } from "../utils/utils";
 
 export class UserController {
@@ -96,6 +97,7 @@ export class UserController {
 
     static test2(req, res) {
         console.log("test2");
-        res.send("test2");
+        res.send("email sent check mailtrap");
+        sendEmail()
     }
 }
